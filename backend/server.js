bb6f1b0fe014feaffed68446909bbca4de31fa2f
MohitChaudhari <mohit.chaudhari@kommunicate.io>
2025-05-01 01:07:56 +0530

    code updated

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: ['http://localhost:5173/api','https://acme-health.vercel.app'],
+  origin: ['http://localhost:5173/api','https://acme-health.vercel.app/api'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
