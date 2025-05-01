a4e107b700c1d049c6896423e746efd7a6ec5e3c
MohitChaudhari <mohit.chaudhari@kommunicate.io>
2025-05-01 11:38:01 +0530

    keep it simple

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,7 @@ import weightRoutes from './routes/weightRoute.js'
 dotenv.config();
 const app = express();
 
-// Manual CORS headers (for stubborn preflight issues)
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://acme-health.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 
